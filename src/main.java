3ca4455def65ad75dcc1560670852c5e2f8048d8
Lewis Mazzei lm297 <Lewis Mazzei lm297@st-andrews.ac.uk>
2018-02-10 15:01:39 +0000

    First attempt worked :) Fixed all the errors

--- src/main.java
+++ src/main.java
@@ -1,8 +1,9 @@
 public class main{
 	public static void main(String[] args){
 		Term london = new Term("London", 100);
-		Term glasgow = new Term("Glasgow", 50);
+		Term glasgow = new Term("London", 50);
 		
-		london.compareTo(glasgow);
+		int val = london.compareTo(glasgow);
+		System.out.println(val);
 	}
 }
