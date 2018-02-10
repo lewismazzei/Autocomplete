0762dbba73783dc99ee3d705597c67c1d2b95cd3
Billy Carlyle <bgtc@st-andrews.ac.uk>
2018-02-10 14:38:53 +0000

    Added some stuff to main

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
 public class main{
 	public static void main(String[] args){
+		Term london = new Term("London", 100);
+		Term glasgow = new Term("Glasgow", 50);
 		
+		london.compareTo(glasgow);
 	}
 }
