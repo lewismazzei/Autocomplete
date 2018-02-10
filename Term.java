27c3e207e180c9505ef03c8168af3e6815232a7d
Billy Carlyle <bgtc@st-andrews.ac.uk>
2018-02-10 14:13:21 +0000

    Created some files

---
+++ Term.java
@@ -0,0 +1,11 @@
+public class Term implements Comparable<Term>{
+	public Term(String query, long weight)
+
+	public static Comparator<Term> byReverseWeightOrder()
+
+	public static Comparator<Term> byPrefixOrder(int r)
+
+	public int compareTo(Term that)
+
+	public String toString()
+}
