97fd076ffeb2956cf283a8f55d9ebbfb54f6f8e5
Billy Carlyle <bgtc@st-andrews.ac.uk>
2018-02-13 16:25:22 +0000

    Attempting to create object list. Not working yet

--- src/Parser.java
+++ src/Parser.java
@@ -4,11 +4,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Parser{
 	static void importData(String fileName){
-		Pattern weightPattern = Pattern.compile("[^\\s](.*)\t");
+		Pattern weightPattern = Pattern.compile("[^\\s](\\d*)\t");
 		Pattern namePattern = Pattern.compile("(?:\t)(.*)");
+		List<Term> cityList = new ArrayList<Term>();
 		try{
 			BufferedReader in = new BufferedReader(new FileReader(fileName));
 			System.out.println("File opened successfully");
@@ -17,16 +20,23 @@ public class Parser{
 				line++;
 				Matcher weightMatcher = weightPattern.matcher(x);
 				Matcher nameMatcher = namePattern.matcher(x);
-				if(weightMatcher.find()){
-					System.out.println(weightMatcher.group(0));
-				}else{
+				if(!weightMatcher.find()){
 					System.out.println("No matches");
-				}
-				if(nameMatcher.find()){
-					System.out.println(nameMatcher.group(0));
 				}else{
+					System.out.println(weightMatcher.group(0));
+				}
+				if(!nameMatcher.find()){
 					System.out.println("No matched names");
 				}
+				if(weightMatcher.find() && nameMatcher.find()){
+					long weight = Long.parseLong(weightMatcher.group(0), 10);
+					Term city = new Term(nameMatcher.group(0), weight);
+					cityList.add(city);
+					System.out.println(city.query);
+				}
+			}
+			for(int i = 0; i < cityList.size(); i++){
+				System.out.println(cityList.get(i).query);
 			}
 		}catch(IOException e){
 			System.out.println("File IO Error!");
--- src/main.java
+++ src/main.java
@@ -1,7 +1,8 @@
+
 public class main{
 	public static void main(String[] args){
 		Term london = new Term("London", 100);
-		Term glasgow = new Term("London", 50);
+		Term glasgow = new Term("Glasgow", 50);
 		
 		int val = london.compareTo(glasgow);
 		System.out.println(val);
