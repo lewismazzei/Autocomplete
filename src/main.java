ed7af7f511aeb75fc407f8e8b1dee05ca48ed814
Billy Carlyle <bgtc@st-andrews.ac.uk>
2018-02-16 17:22:18 +0000

    Started byReverseWeightOrder, not sure why it won't compile

--- src/main.java
+++ src/main.java
@@ -1,11 +1,16 @@
-
+import java.util.Collections;
+import java.util.List;
 public class main{
 	public static void main(String[] args){
 		Term london = new Term("London", 100);
 		Term glasgow = new Term("Glasgow", 50);
 		
-		int val = london.compareTo(glasgow);
-		System.out.println(val);
-		Parser.importData("cities.txt");
+		//int val = london.compareTo(glasgow);
+		//System.out.println(val);
+		//Parser.importData("cities.txt");
+		List<Term> terms = new ArrayList<Term>();
+		terms.add(london);
+		terms.add(glasgow);
+		Collections.sort(terms, Term.byReverseWeightOrder());
 	}
 }
