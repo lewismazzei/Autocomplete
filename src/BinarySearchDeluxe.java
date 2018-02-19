1dc89d227d4a041615b99f0827a1ac5a31f757b2
Lewis Mazzei lm297 <Lewis Mazzei lm297@st-andrews.ac.uk>
2018-02-19 19:53:40 +0000

    Binary search works now (I think)

--- src/BinarySearchDeluxe.java
+++ src/BinarySearchDeluxe.java
@@ -1,75 +1,64 @@
-import java.util.ArrayList;
 import java.util.Comparator;
 
 public class BinarySearchDeluxe {
 
-    // Returns the index of the first key in a[] that
-    // equals the search key, or -1 if no such key.
+    //returns the index of the first key in a[] that equals the search key, or -1 if no such key.
     public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
-        ArrayList<Integer> keyIndices = findKeyIndices(a, key, comparator);
-
-        int firstIndex = a.length - 1;
-
-        if (keyIndices.size() != 0) {
-            for (int keyIndex : keyIndices) {
-                if (keyIndex < firstIndex) {
-                    firstIndex = keyIndex;
-                }
-            }
+        if (a == null || key == null || comparator == null) {
+            throw new NullPointerException();
         }
-        return firstIndex;
+
+        int index = binarySearch(a, key, comparator);
+
+        if (index >= 0) {
+            while (comparator.compare(a[index], a[index - 1]) == 0 && index > 0) {
+                index--;
+            }
+            return index;
+        } else {
+            return -1;
+        }
     }
 
-    // Returns the index of the last key in a[] that
-    //equals the search key, or -1 if no such key.
+    //returns the index of the last key in a[] that equals the search key, or -1 if no such key.
     public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
-        ArrayList<Integer> keyIndices = findKeyIndices(a, key, comparator);
-
-        int lastIndex = 0;
-
-        if (keyIndices.size() != 0) {
-            for (int keyIndex : keyIndices) {
-                if (keyIndex > lastIndex) {
-                    lastIndex = keyIndex;
-                }
-            }
+        if (a == null || key == null || comparator == null) {
+            throw new NullPointerException();
         }
-        return lastIndex;
+
+        int index = binarySearch(a, key, comparator);
+
+        if (index >= 0) {
+            while (comparator.compare(a[index], a[index+1]) == 0 && index < a.length-2) {
+                index++;
+            }
+            return index;
+        } else {
+            return -1;
+        }
     }
 
+    //binary search implementation using custom comparator
     public static <Key> int binarySearch(Key[] a, Key key, Comparator<Key> comparator) {
+        if (a == null || key == null || comparator == null) {
+            throw new NullPointerException();
+        }
+
         int lowIndex = 0;
         int middleIndex;
         int highIndex = a.length;
 
-        while ((lowIndex + 1) < highIndex) {
+        while (lowIndex <= highIndex) {
             middleIndex = (highIndex + lowIndex) / 2;
-            if (comparator.compare(key, a[middleIndex]) < 0) {
-                highIndex = middleIndex;
-            } else if (comparator.compare(key, a[middleIndex]) > 0) {
-                System.out.println(key.toString() + " : " + a[middleIndex].toString());
-                lowIndex = middleIndex;
-            } else {
 
+            if (comparator.compare(key, a[middleIndex]) < 0) {
+                highIndex = middleIndex - 1;
+            } else if (comparator.compare(key, a[middleIndex]) > 0) {
+                lowIndex = middleIndex + 1;
+            } else {
                 return middleIndex;
             }
         }
         return -1;
-    }
-
-    public static <Key> ArrayList<Integer> findKeyIndices(Key[] a, Key key, Comparator<Key> comparator) {
-        ArrayList<Integer> keyIndices = new ArrayList<>();
-
-        while (true) {
-            int index = binarySearch(a, key, comparator);
-
-            if (index >= 0) {
-                keyIndices.add(index);
-                a[index] = null;
-            } else {
-                break;
-            }
-        }
-        return keyIndices;
     }
 }
