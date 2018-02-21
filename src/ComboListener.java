3ba22d4c8d3d61ebadf64e4fa517fc5d6fdef005
Billy Carlyle <bgtc@st-andrews.ac.uk>
2018-02-21 16:19:41 +0000

    Fixed combobox issue

--- src/ComboListener.java
+++ src/ComboListener.java
@@ -23,16 +23,18 @@ public class ComboListener extends KeyAdapter{
 				String text = ((JTextField)key.getSource()).getText();
 				Term[] matches = autocomplete.allMatches(text);
 
+				listener.setSelectedIndex(-1);
 
                 try {
 					gui.setResults(matches.length + 1, k);
                     listener.setModel(new DefaultComboBoxModel(Arrays.copyOfRange(matches, 0, k)));
                 } catch (NullPointerException e) {
-					System.out.println(e.getMessage());
+					listener.setModel(new DefaultComboBoxModel());
 					listener.hidePopup();
+					gui.setResults(0, k);
+					System.out.println(e.getMessage());
                 }
 				//Set selected index as -1
-				listener.setSelectedIndex(-1);
 				((JTextField)listener.getEditor().getEditorComponent()).setText(text);
 				if(!text.equals("")){
 					listener.showPopup();
