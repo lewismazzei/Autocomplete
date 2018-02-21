72d1b12401a83109a43d36a91050db07fb454c7d
Lewis Mazzei lm297 <Lewis Mazzei lm297@st-andrews.ac.uk>
2018-02-21 19:18:11 +0000

    Added some exceptions

--- src/Term.java
+++ src/Term.java
@@ -16,18 +16,22 @@ public class Term implements Comparable<Term>{
 
 	//compare two terms by weight: calculates the difference so the order can be determined by the sign of the result
 	public static Comparator<Term> byReverseWeightOrder() {
+		//returns a negative, positive or 0 value integer which indicates the order of the terms
 	    return (term1, term2) -> (int)(term2.weight - term1.weight);
 	}
 
 	//compare two terms by prefix order: compares lowercase substrings of two term queries (the length the substring is determined by
 	//the argument passed) lexicographically to determine their order
 	public static Comparator<Term> byPrefixOrder(int r) {
+			//any value less than 0 is invalid, throw an exception if this occurs
+			if (r < 0) throw new IllegalArgumentException();
+			//returns a negative, positive or 0 value integer which indicates the order of the terms
             return (term1, term2) -> (int)(term1.query.substring(0,r).toLowerCase().compareTo(term2.query.substring(0,r).toLowerCase()));
-
     }
 
     //compares two terms lexicographically to determine their order
 	public int compareTo(Term that) {
+		//returns a negative, positive or 0 value integer which indicates the order of the terms
 		return this.query.toLowerCase().compareTo(that.query.toLowerCase());
 	}
 
